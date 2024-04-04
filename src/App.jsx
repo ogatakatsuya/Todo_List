4c82c85935c417f0205158c8084f10d8448509a9
ogatakatsuya <katsuya151225@icloud.com>
2024-04-04 22:04:52 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Suspense, useState, useEffect, lazy } from "react";
-import Example from '../Todo/Example';
+import Example from './Todo/Example';
 
 const STORAGE_KEY = "rcg-current-lec-index";
 
